a53daa4bb35caf4c2c7013da88103eea206eff3d
Dmitrii Posadskii <dmitry.posadsky@gmail.com>
2019-08-24 12:08:47 +0300

    [SR-29] Error handling in requests

--- src/common/URL.js
+++ src/common/URL.js
@@ -3,7 +3,6 @@ const SERVER_URL = 'https://server.posadskiy.space/';
 const USER_MAIN = SERVER_URL + 'user/';
 
 const USER = {
-	USER_BY_ID: (id) =>  USER_MAIN + "id/" + id,
 	USER_BY_NAME: (name) => `${USER_MAIN}name/${name}`,
 	USER_ALL: USER_MAIN + 'all/',
 	AUTH: USER_MAIN + 'auth/',
